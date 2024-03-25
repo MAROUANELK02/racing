f3d4b88200914bf8c5376dc3fce7fcb282814e8f
Marouane LASMAK <116379216+MAROUANELK02@users.noreply.github.com>
2024-03-25 00:14:27 +0000

    v1

---
+++ app/src/main/java/com/example/racetracker/ui/RaceTrackerApp.kt
@@ -0,0 +1,195 @@
+package com.example.racetracker.ui
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.example.racetracker.R
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+
+@Composable
+fun RaceTrackerApp() {
+    val playerOne = remember {
+        RaceParticipant(name = "Player 1", progressIncrement = 1)
+    }
+    val playerTwo = remember {
+        RaceParticipant(name = "Player 2", progressIncrement = 2)
+    }
+    var raceInProgress by remember { mutableStateOf(false) }
+
+    if (raceInProgress) {
+        LaunchedEffect(playerOne, playerTwo) {
+            coroutineScope {
+                launch { playerOne.run() }
+                launch { playerTwo.run() }
+            }
+            raceInProgress = false
+        }
+    }
+    RaceTrackerScreen(
+        playerOne = playerOne,
+        playerTwo = playerTwo,
+        isRunning = raceInProgress,
+        onRunStateChange = { raceInProgress = it },
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(horizontal = dimensionResource(R.dimen.padding_medium)),
+    )
+}
+
+@Composable
+private fun RaceTrackerScreen(
+    playerOne: RaceParticipant,
+    playerTwo: RaceParticipant,
+    isRunning: Boolean,
+    onRunStateChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(R.string.run_a_race),
+            style = MaterialTheme.typography.headlineSmall,
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(dimensionResource(R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            StatusIndicator(
+                participantName = playerOne.name,
+                currentProgress = playerOne.currentProgress,
+                maxProgress = stringResource(
+                    R.string.progress_percentage,
+                    playerOne.maxProgress
+                ),
+                progressFactor = playerOne.progressFactor,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.size(dimensionResource(R.dimen.padding_large)))
+            StatusIndicator(
+                participantName = playerTwo.name,
+                currentProgress = playerTwo.currentProgress,
+                maxProgress = stringResource(
+                    R.string.progress_percentage,
+                    playerTwo.maxProgress
+                ),
+                progressFactor = playerTwo.progressFactor,
+                modifier = Modifier.fillMaxWidth(),
+            )
+            Spacer(modifier = Modifier.size(dimensionResource(R.dimen.padding_large)))
+            RaceControls(
+                isRunning = isRunning,
+                onRunStateChange = onRunStateChange,
+                onReset = {
+                    playerOne.reset()
+                    playerTwo.reset()
+                    onRunStateChange(false)
+                },
+                modifier = Modifier.fillMaxWidth(),
+            )
+        }
+    }
+}
+
+@Composable
+private fun StatusIndicator(
+    participantName: String,
+    currentProgress: Int,
+    maxProgress: String,
+    progressFactor: Float,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+    ) {
+        Box(
+            modifier = Modifier
+                .size(dimensionResource(R.dimen.icon_size))
+                .padding(end = dimensionResource(R.dimen.padding_small))
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_walk),
+                contentDescription = null,
+                modifier = Modifier.size(dimensionResource(R.dimen.icon_size))
+                    .padding(end = dimensionResource(R.dimen.padding_small)),
+            )
+            // Calcul de la position de l'icône en fonction du progrès
+            val iconPosition = (progressFactor * 100).toInt().coerceIn(0, 100)
+            Icon(
+                painter = painterResource(R.drawable.ic_walk),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(dimensionResource(R.dimen.icon_size))
+                    .offset(x = iconPosition.dp)
+            )
+        }
+        Spacer(modifier = Modifier.size(dimensionResource(R.dimen.padding_large)))
+        Column(
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
+        ) {
+            LinearProgressIndicator(
+                progress = progressFactor,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(dimensionResource(R.dimen.progress_indicator_height))
+            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = stringResource(R.string.progress_percentage, currentProgress),
+                    textAlign = TextAlign.Start,
+                    modifier = Modifier.weight(1f)
+                )
+                Text(
+                    text = maxProgress,
+                    textAlign = TextAlign.End,
+                    modifier = Modifier.weight(1f)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun RaceControls(
+    onRunStateChange: (Boolean) -> Unit,
+    onReset: () -> Unit,
+    modifier: Modifier = Modifier,
+    isRunning: Boolean = true,
+) {
+    Column(
+        modifier = modifier.padding(top = dimensionResource(R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium))
+    ) {
+        Button(
+            onClick = { onRunStateChange(!isRunning) },
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text(if (isRunning) stringResource(R.string.pause) else stringResource(R.string.start))
+        }
+        OutlinedButton(
+            onClick = onReset,
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text(stringResource(R.string.reset))
+        }
+    }
+}
